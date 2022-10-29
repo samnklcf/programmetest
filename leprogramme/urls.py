9a02afdd1c82015a8a78f84b5594762cbcdbffb3
samnklcf <samyeyebe@gmail.com>
2022-10-30 00:55:51 +0100

    samuel

--- leprogramme/urls.py
+++ leprogramme/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cours/', include('cours.urls')),
+    path('', include('cours.urls')),
     path("ckeditor/", include('ckeditor_uploader.urls'), name=""),
 ]
 
